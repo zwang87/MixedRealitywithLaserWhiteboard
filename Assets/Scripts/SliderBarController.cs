6d6eccd868cfa61cd0a571319ceea1a13cac3468
zwang87 <zw401@nyu.edu>
2014-01-23 18:45:43 -0500

    Mixed Reality with Laser whiteboard
    laser whiteboard implemented in Mixed Reality, supposed to run with
    LaserSpotwithUDP project.

---
+++ Assets/Scripts/SliderBarController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderBarController : MonoBehaviour {
+	public double minV = -0.5, maxV = 0.5, curV = 0;
+
+	public enum SliderType{
+		XSlider = 0,
+		YSlider = 1,
+		ZSlider = 2
+	};
+
+	public SliderType sliderType;// = SliderType.XSlider;
+	GameObject psMove;
+	Vector3 localPos;
+	// Use this for initialization
+	void Start () {
+		//this.transform.tag = "SliderBar";
+		psMove = GameObject.Find("PSMoveController");
+		localPos = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		localPos = this.transform.parent.transform.InverseTransformPoint(psMove.transform.position);
+		//Debug.Log(localPos.x + " " + localPos.y + " " + localPos.z);
+		clampBarPosition();
+	}
+
+	private void clampBarPosition(){
+		Vector3 barPos = this.transform.localPosition;
+		switch(sliderType){
+		case SliderType.XSlider:
+			barPos.x = Mathf.Clamp (localPos.x, (float)-maxV, (float)maxV);
+			barPos.y = 0;
+			barPos.z = 0;
+			break;
+		case SliderType.YSlider:
+			barPos.y = 0;
+			barPos.x = Mathf.Clamp (localPos.x, (float)-maxV, (float)maxV);
+			barPos.z = 0;
+			break;
+		case SliderType.ZSlider:
+			barPos.x = 0;
+			barPos.y = 0;
+			barPos.z = Mathf.Clamp (localPos.z, (float)-maxV, (float)maxV);
+			break;
+		}
+
+		this.transform.localPosition = barPos;
+	}
+}
---
+++ Assets/Scripts/UDPReceiver.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceiver : MonoBehaviour {
+	Thread receiveThread;
+	UdpClient udpClient;
+	public const int port = 9090;
+	public string lastReceivedUDPpacket = "";
+
+	private double posX = 0;
+	private double posY = 0;
+	private string content = "";
+	private System.Object thisLock = new System.Object();
+
+	// Use this for initialization
+	void Start () {
+		receiveThread = new Thread(new ThreadStart(this.ReceiveData));
+		receiveThread.IsBackground = true;
+		receiveThread.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(posX + "  " + posY);
+		if(posX >= 0 && posY >= 0)
+			this.transform.localPosition = new Vector3((float)(5*(posX-160.0)/160), 0, (float)(5*(120.0-posY)/120));
+	}
+
+	private void ReceiveData(){
+		udpClient = new UdpClient(port);
+
+		while(true){
+			try{
+				IPEndPoint ip = new IPEndPoint(IPAddress.Any, 0);
+				byte[] data = udpClient.Receive(ref ip);
+				lock(thisLock){
+					content = Encoding.UTF8.GetString (data);
+					string[] array = content.Split(' ');
+					if("pos:" == array[0]){
+						posX = Convert.ToDouble(array[1]);
+						posY = Convert.ToDouble(array[2]);
+					}
+				}
+			}catch(Exception e){
+				Debug.Log(e.ToString());
+			}
+		}
+		udpClient.Close();
+	}
+
+	void OnDisable(){
+		if(receiveThread != null)
+			receiveThread.Abort();
+		Debug.Log("Connection Lost");
+	}
+}
